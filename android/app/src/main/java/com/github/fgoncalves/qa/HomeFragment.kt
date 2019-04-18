264dcee0faa3d0ef7ff2a3cb5fd880cec89491a3
Robin <rburgess@babbel.com>
2019-04-18 15:56:52 +0200

    add android app

---
+++ android/app/src/main/java/com/github/fgoncalves/qa/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.github.fgoncalves.qa
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.home_screen, container, false)
+}
